733d3108f21b1105f9687afd18661672c0bb993e
Leonardo <leonardo_vieira199@hotmail.com>
2026-05-22 08:23:29 -0300

    acess control, by role, by route

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$12$UYZjmNAWR5XyHFzlCW341.LbV9iLmsb2tMY0Dz0BM67Nsq82FSpI2');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$12$UYZjmNAWR5XyHFzlCW341.LbV9iLmsb2tMY0Dz0BM67Nsq82FSpI2');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
